dcbca8a9a7b304524aa6ed107a343b7b7bed4eb7
lomin <happylomin@gmail.com>
2020-02-12 15:15:55 +0100

    Added GoldenMasterTest skeleton.

---
+++ src/test/java/de/itagile/GoldenMasterTest.java
@@ -0,0 +1,13 @@
+package de.itagile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GoldenMasterTest {
+
+    @Test
+    public void currencyConverter() {
+        assertEquals("golden", "master");
+    }
+}
