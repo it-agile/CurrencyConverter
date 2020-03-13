5ec6fe0f8a523619a5975faba19979c23215969d
Björn Ostermann <bjoern.ostermann@it-agile.de>
2020-03-13 14:19:20 +0100

    Add new Test

--- src/test/java/de/itagile/CurrencyCalcTest.java
+++ src/test/java/de/itagile/CurrencyCalcTest.java
@@ -25,6 +25,23 @@ public class CurrencyCalcTest {
                         DKK));
     }
 
+    @Test
+    public void convertCurrenciesWithFee() {
+        // given
+        ConversionRateRetriever retriever = conversionRateFor1EurIs(7, DKK);
+
+        // when
+        CurrencyConverter currencyConverter = new CurrencyConverter(retriever);
+
+        // then
+        assertEquals(money(create(7), DKK),
+                currencyConverter.convert(
+                        money(create(1), EUR),
+                        DKK));
+    }
+
+
+
     private ConversionRateRetriever conversionRateFor1EurIs(final double amount, final Currency currency) {
         return () -> new ExchangeRateService() {
             @Override
