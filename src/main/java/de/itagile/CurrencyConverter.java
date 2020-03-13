3787388f9e622113016b3dd70403583bd1116aa0
Andreas Havenstein <ah@it-agile.de>
2020-03-13 14:39:51 +0100

    Create Percent class

--- src/main/java/de/itagile/CurrencyConverter.java
+++ src/main/java/de/itagile/CurrencyConverter.java
@@ -12,8 +12,4 @@ public class CurrencyConverter {
         return money.change(exchangeRateService.getExchangeRate(money.getCurrency(), destCurrency));
     }
 
-
-    public <DestCurrency extends Currency> Money<DestCurrency> convert(Money<Eur> money, DestCurrency destCurrency, String date, Money<Eur> fee) {
-        return convert(money, destCurrency);
-    }
 }
---
+++ src/test/java/de/itagile/PercentTest.java
@@ -0,0 +1,4 @@
+package de.itagile;
+
+public class PercentTest {
+}
